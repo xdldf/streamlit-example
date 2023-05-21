2a7e414026e54b1ebc2c4638ead3e7ec91ce37db
xdldf <118965676+xdldf@users.noreply.github.com>
2023-05-21 19:25:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,21 @@
 import streamlit as st
-import plotly.express as px
-import pandas as pd
+import plotly.graph_objs as go
 
-# Создаем DataFrame с данными о безработице
-data = {'Year': [2022, 2023],
-        'Unemployment': [5.0, 4.5]}
-df = pd.DataFrame(data)
+# Создание данных для графика
+x_values = list(range(10))
+y_values = list(range(10))
 
-# Добавляем виджет для выбора значения безработицы
-unemployment = st.slider('Select Unemployment Rate', min_value=0.0, max_value=10.0, value=5.0)
+# Создание графика с помощью Plotly
+fig = go.FigureWidget([go.Scatter(x=x_values, y=y_values, mode='markers')])
 
-# Обновляем данные в DataFrame
-df.loc[df['Year'] == 2022, 'Unemployment'] = unemployment
+# Функция для обновления данных на графике при перетаскивании точек мышью
+def update_point(trace, points, selector):
+    for i in points.point_inds:
+        x_values[i] = points.xs[i]
+        y_values[i] = points.ys[i]
 
-# Создаем интерактивный график с плавающими точками
-fig = px.scatter(df, x='Year', y='Unemployment', title='Unemployment Rate')
-fig.update_traces(mode='markers+lines')
+# Добавление обработчика событий для обновления данных на графике при перетаскивании точек мышью
+fig.data[0].on_click(update_point)
+
+# Отображение графика в Streamlit
 st.plotly_chart(fig)
