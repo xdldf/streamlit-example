1f546f8084136f5a451447363eb8124af492bfd7
xdldf <118965676+xdldf@users.noreply.github.com>
2023-05-21 19:20:58 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,19 @@
 import streamlit as st
-import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
 
-fig = go.Figure()
+# Создаем DataFrame с данными о безработице
+data = {'Year': [2022, 2023],
+        'Unemployment': [5.0, 4.5]}
+df = pd.DataFrame(data)
 
-for i in range(10):
-    fig.add_trace(go.Scatter(x=[i], y=[i], mode='markers', marker=dict(size=10)))
+# Добавляем виджет для выбора значения безработицы
+unemployment = st.slider('Select Unemployment Rate', min_value=0.0, max_value=10.0, value=5.0)
 
-fig.update_layout(
-    xaxis=dict(range=[-1, 11]),
-    yaxis=dict(range=[-1, 11]),
-    dragmode='pan',
-    hovermode='closest'
-)
+# Обновляем данные в DataFrame
+df.loc[df['Year'] == 2022, 'Unemployment'] = unemployment
 
+# Создаем интерактивный график с плавающими точками
+fig = px.scatter(df, x='Year', y='Unemployment', title='Unemployment Rate')
+fig.update_traces(mode='markers+lines')
 st.plotly_chart(fig)
