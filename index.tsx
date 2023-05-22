8d9f1eabf70dc246dc42d09f7307b107534c2890
xdldf <118965676+xdldf@users.noreply.github.com>
2023-05-22 17:51:13 +0900

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import StreamlitChart from "./StreamlitChart";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StreamlitChart />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
