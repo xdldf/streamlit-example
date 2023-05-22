d380d4d17cdd2a39c1629b6740191e0f4a31a9ba
xdldf <118965676+xdldf@users.noreply.github.com>
2023-05-22 13:26:49 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ with open('new_model.pkl', 'rb') as model_pkl:
     lr = pd.read_pickle(model_pkl)
 
 # Create a text input for the user to enter the value for the unseen variable
-unseen = st.text_input('Enter the value for the unseen variable:', 'введите кол-во алкашей (в тыс. чел)')
+unseen = st.text_input('Enter the value for the unseen variable:', Null)
 
 # Convert the input to a float and create a test observation
 X_test_sm = [[float(1.0)], [float(unseen)]]
@@ -17,4 +17,7 @@ X_test_sm = np.squeeze(X_test_sm)
 result = lr.predict(X_test_sm)[0]
 
 # Display the result
-st.write(f'При количестве безработных {float(unseen)} тыс. человек, количество алкоголиков будет составлять {result} тыс. человек')
+if result < 0:
+    st.write(f'При количестве безработных в 0 тыс. человек, алкоголиков не будет')
+else:
+    st.write(f'При количестве безработных {float(unseen)} тыс. человек, количество алкоголиков будет составлять {result} тыс. человек')
